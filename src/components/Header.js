b90ba0e8652c236be347f8aad45fcf34bb27f2db
anshu <manu22425@gmail.com>
2023-12-06 02:39:02 +0530

    Created Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-gradient-to-b from-black'>
+      <img  className='h-42 w-56' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'/>
+    </div>
+  )
+}
+
+export default Header
