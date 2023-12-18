5f38cf4f9dbead189743b734655b1209a3a41095
anshu <manu22425@gmail.com>
2023-12-19 00:39:23 +0530

    Validation Done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { checkValidData } from '../utils/validate';
 
 const Login = () => {
     const [signUpMode, setSignUpMode] = useState(false);
+    const [validationError, setValidationError] = useState(null);
     const handleSignUpMode = () => {
         if(signUpMode){
             setSignUpMode(false);
@@ -10,28 +12,33 @@ const Login = () => {
             setSignUpMode(true);
         }
     }
-    const handleSubmit = () => {
-        console.log("submitted")
+    const email = useRef(null);
+    const password = useRef(null);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(password.current.value);
+        const validData = checkValidData(email.current.value, password.current.value);
+        setValidationError(validData);
     }
     return (
         <div className="w-full max-w-xs mx-auto">
-            <form className="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4 text-white bg-opacity-75">
+            <form className="bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4 text-white bg-opacity-75" onSubmit={handleSubmit}>
                 <p className='text-2xl mt-4 mb-4'>{signUpMode ?  'Sign Up' : "Sign In"}</p>
-
                 {signUpMode && (
                     <div className="my-8">
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Name" />
                 </div>
                 )}
                 <div className="my-8">
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email Address" />
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="email" ref={email} type="text" placeholder="Email Address" />
                 </div>
                 
                 <div className="my-8">
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-800 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" />
+                    <input className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-800 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" ref={password} type="password" placeholder="Password" />
                 </div>
+                {validationError ? (<div className='text-red-500 mb-2'>{validationError}</div>) : null}
                 <div className="flex items-center justify-between">
-                    <button className="w-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleSubmit}>
+                    <button className="w-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                     {signUpMode ?  'Create Account' : "Sign In"}
                     </button>
                 </div>
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,3 +1,11 @@
 export const checkValidData = (email , password) => {
-    
+    const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    const isPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d!@#$%^&*()_+]{8,}$/.test(password);
+
+
+    if(!isEmailValid) return "Email ID is not valid";
+    if(!isPassword) return "Password is not valid";
+
+    return null;
+
 }
