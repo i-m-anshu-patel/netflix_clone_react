479fd1c86c553ff904f6a79a0556634927880dda
anshu <manu22425@gmail.com>
2023-12-26 01:48:07 +0530

    Video BackGround

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import useFetchData from '../utils/useFetchData';
 import { MOVIE_DETAILS_PART1, MOVIE_DETAILS_PART2 } from '../utils/constants';
+import VideoBackGround from './VideoBackGround';
 
 const MainContainer = ({ movie }) => {
     const [mainMovieDetail, setMainMovieDetail] = useState([])
@@ -12,18 +13,25 @@ const MainContainer = ({ movie }) => {
             setMainMovieDetail(movieDetail.results)
         }
     }, [movieDetail])
-    return mainMovieDetail && (
+    return mainMovieDetail[0] && (
+        <>
+        <VideoBackGround videoKey={mainMovieDetail[0].key}/>
         <div className='grid grid-cols-2 pl-2'>
             <div className='m-16 pt-20'>
                 <p className='text-3xl'>{movie.original_title}</p>
                 <p className='text-lg pt-2'>{movie.overview}</p>
+                <div className='flex'>
                 <button className='mt-3'>Play Now</button>
-                <p>{mainMovieDetail[1].type}</p>
+                <button className='mt-3'>More info</button>
+                </div>
+                
+                <iframe src={"https://www.youtube.com/embed/"+mainMovieDetail[0].key+"?autoplay=1&mute=1"} title="Trailer" frameBorder="0"></iframe>
             </div>
             <div>
                 Side wala
             </div>
         </div>
+        </>
     )
 }
 
---
+++ src/components/VideoBackGround.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoBackGround = () => {
+  return (
+    <div>
+      VideoBackGround
+    </div>
+  )
+}
+
+export default VideoBackGround
