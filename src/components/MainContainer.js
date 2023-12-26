19440e4001b5ad75a7d0d03f8a3140985de98c16
anshu <manu22425@gmail.com>
2023-12-26 23:10:54 +0530

    CSS of mainContainer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -6,32 +6,29 @@ import VideoBackGround from './VideoBackGround';
 const MainContainer = ({ movie }) => {
     const [mainMovieDetail, setMainMovieDetail] = useState([])
     const movieId = movie.id;
-    const movieDetail = useFetchData(MOVIE_DETAILS_PART1+movieId+MOVIE_DETAILS_PART2)
+    const movieDetail = useFetchData(MOVIE_DETAILS_PART1 + movieId + MOVIE_DETAILS_PART2)
 
     useEffect(() => {
-        if(movieDetail && movieDetail.results){
+        if (movieDetail && movieDetail.results) {
             setMainMovieDetail(movieDetail.results)
         }
     }, [movieDetail])
     return mainMovieDetail[0] && (
-        <>
-        <VideoBackGround videoKey={mainMovieDetail[0].key}/>
-        <div className='grid grid-cols-2 pl-2'>
-            <div className='m-16 pt-20'>
-                <p className='text-3xl'>{movie.original_title}</p>
-                <p className='text-lg pt-2'>{movie.overview}</p>
-                <div className='flex'>
-                <button className='mt-3'>Play Now</button>
-                <button className='mt-3'>More info</button>
+        <div className='relative'>
+            <VideoBackGround videoKey={mainMovieDetail[0].key} />
+            <div className='grid grid-cols-2 pl-2 aspect-video bg-gradient-to-r from-black text-white absolute top-0 left-0'>
+                <div className='m-16 pt-36'>
+                    <p className='text-3xl'>{movie.original_title}</p>
+                    <p className='text-lg pt-2'>{movie.overview}</p>
+                    <div className='flex'>
+                        <button className='mt-3'>Play Now</button>
+                        <button className='mt-3 ml-2'>More info</button>
+                    </div>
                 </div>
-                
-                <iframe src={"https://www.youtube.com/embed/"+mainMovieDetail[0].key+"?autoplay=1&mute=1"} title="Trailer" frameBorder="0"></iframe>
-            </div>
-            <div>
-                Side wala
+                <div>
+                </div>
             </div>
         </div>
-        </>
     )
 }
 
--- src/components/VideoBackGround.js
+++ src/components/VideoBackGround.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const VideoBackGround = () => {
+const VideoBackGround = ({videoKey}) => {
   return (
-    <div>
-      VideoBackGround
+    <div className='aspect-video overflow-hidden'>
+      <iframe className='w-full h-full ' src={"https://www.youtube.com/embed/"+videoKey+"?autoplay=1&mute=1&controls=0&&showinfo=0&loop=1"} title="Trailer" frameBorder="0"></iframe>
     </div>
   )
 }
